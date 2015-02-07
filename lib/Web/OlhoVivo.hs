1e0e0ff78a7634b806232ec21741ef27137353c9
yamadapc <tacla.yamada@gmail.com>
2015-02-07 20:35:01 -0200

    Start a major review on the wrapper's structure
    I'll probably start to lean a little more towards something like what
    we did in `hzulip` [1]. Though I didn't really want to use a Monad
    Transformer here right away.
    
    [1] - https://github.com/yamadapc/hzulip

--- lib/Web/OlhoVivo.hs
+++ lib/Web/OlhoVivo.hs
@@ -14,11 +14,13 @@
 -- You should have received a copy of the GNU General Public License along
 -- with this program; if not, write to the Free Software Foundation, Inc.,
 -- 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+{-# LANGUAGE DeriveDataTypeable #-}
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE TemplateHaskell #-}
 module Web.OlhoVivo where
 
 import Control.Applicative ((<$>))
+import Control.Exception (Exception, throwIO)
 import Control.Lens ((^.), (^?))
 import Data.Aeson (FromJSON, Result(..), Object, fromJSON)
 import Data.Aeson.TH (Options(..), defaultOptions)
@@ -27,10 +29,13 @@ import Data.ByteString.Lazy (ByteString)
 import Data.Char (toLower)
 import Data.Default (Default(..))
 import qualified Data.HashMap.Strict as HashMap (lookup)
+import Data.Typeable (Typeable)
 import Data.Text (Text, pack)
 import Network.Wreq (asJSON, defaults, responseBody)
+import Network.Wreq.Lens (statusCode)
 import Network.Wreq.Types (Options(..))
-import Network.Wreq.Session
+import Network.Wreq.Session hiding (withSession)
+import qualified Network.Wreq.Session as Wreq (withSession)
 
 import Web.OlhoVivo.TH
 
@@ -92,6 +97,36 @@ instance Default OlhoVivoApiOptions where
             , olhovivoApiVersion = "0"
             }
 
+data OlhoVivoError = AuthenticationError
+                   | UnknownError Int String
+  deriving(Show, Typeable)
+
+instance Exception OlhoVivoError
+
+-- |
+-- Authenticates an existing Wreq `Session`. Throws `AuthenticationError` on
+-- failure.
+authenticateSession :: OlhoVivoApiOptions -> Text -> Session -> IO ()
+authenticateSession opts token session = do
+    let url = urlForEndpoint opts "/Login/Autenticar"
+        reqOpts = defaults { params = [ ("token", token)
+                                      ]
+                            }
+    res <- postWith reqOpts session url ("" :: ByteString)
+    -- print $ res ^. statusCode
+    case res ^? responseBody of
+       Just "true" -> return ()
+       Just "false" -> throwIO AuthenticationError
+       _ -> throwIO AuthenticationError
+
+-- |
+-- Like `Network.Wreq.Session.withSession` but for authenticated Olho Vivo API
+-- Sessions.
+withSession :: OlhoVivoApiOptions -> Text -> (Session -> IO a) -> IO a
+withSession opts token action = Wreq.withSession $ \session -> do
+    authenticateSession opts token session
+    action session
+
 -- |
 -- Authenticates a Wreq 'Session' with the OlhoVivo API given a set of API
 -- Options and an application token.
@@ -114,23 +149,14 @@ newOlhoVivoApi session opts token = do
        Just "false" -> return False
        _ -> return False
 
-olhoVivoGet :: FromJSON a => Session -> OlhoVivoApiOptions -> String
-            -> Network.Wreq.Types.Options -> IO a
-olhoVivoGet session opts endpoint reqOpts = do
-    let url = urlForEndpoint opts endpoint
-    json <- asJSON =<< getWith reqOpts session url
-    return $ json ^. responseBody
-
 olhoVivoLines :: Session -> OlhoVivoApiOptions -> Text -> IO [OlhoVivoLine]
 olhoVivoLines session opts q =
-    let reqOpts = defaults { params = [ ("termosBusca", q) ]
-                           }
+    let reqOpts = defaults { params = [ ("termosBusca", q) ] }
       in olhoVivoGet session opts "/Linha/Buscar" reqOpts
 
 olhoVivoStops :: Session -> OlhoVivoApiOptions -> Text -> IO [OlhoVivoStop]
 olhoVivoStops session opts q =
-    let reqOpts = defaults { params = [ ("termosBusca", q) ]
-                           }
+    let reqOpts = defaults { params = [ ("termosBusca", q) ] }
       in olhoVivoGet session opts "/Parada/Buscar" reqOpts
 
 olhoVivoExpressLanes :: Session -> OlhoVivoApiOptions -> IO [OlhoVivoExpressLane]
@@ -140,8 +166,7 @@ olhoVivoExpressLanes session opts =
 
 olhoVivoStopsInLine :: Session -> OlhoVivoApiOptions -> LineCode -> IO [OlhoVivoStop]
 olhoVivoStopsInLine session opts lineCode =
-    let reqOpts = defaults { params = [ ("codigoLinha", pack (show lineCode)) ]
-                           }
+    let reqOpts = defaults { params = [ ("codigoLinha", pack (show lineCode)) ] }
       in olhoVivoGet session opts "/Parada/BuscarParadasPorLinha" reqOpts
 
 olhoVivoStopsInExpressLane :: Session -> OlhoVivoApiOptions -> Int
@@ -167,3 +192,10 @@ olhoVivoLinePositions session opts lineCode = do
 urlForEndpoint :: OlhoVivoApiOptions -> String -> String
 urlForEndpoint opts endpoint =
     olhovivoApiBaseUrl opts ++ olhovivoApiVersion opts ++ endpoint
+
+olhoVivoGet :: FromJSON a => Session -> OlhoVivoApiOptions -> String
+            -> Network.Wreq.Types.Options -> IO a
+olhoVivoGet session opts endpoint reqOpts = do
+    let url = urlForEndpoint opts endpoint
+    json <- asJSON =<< getWith reqOpts session url
+    return $ json ^. responseBody
--- test/Web/OlhoVivoSpec.hs
+++ test/Web/OlhoVivoSpec.hs
@@ -2,66 +2,63 @@
 module Web.OlhoVivoSpec
   where
 
+import Control.Lens ((^.))
 import Data.Default (def)
 import Data.String (fromString)
+import Data.Text (Text)
 import Network.Wreq (defaults)
-import Network.Wreq.Session
+import Network.Wreq.Lens (responseStatus, statusCode)
+import Network.Wreq.Session hiding (withSession)
+import qualified Network.Wreq.Session as Wreq (withSession)
 import Network.Wreq.Types (Options(..))
 import Test.Hspec (Spec, describe, it, shouldBe, shouldSatisfy)
 import System.Environment (getEnv)
 import System.IO
+import System.IO.Unsafe (unsafePerformIO)
 import Web.OlhoVivo
+
+token :: Text
+token = unsafePerformIO $ fromString `fmap` getEnv "SPTRANS_TOKEN"
+
+withTestSession = withSession def token
 
 spec :: Spec
 spec = do
-    describe "newOlhoVivoApi" $
-        it "authenticates a wreq session with the Olho Vivo API" $
-            withSession $ \session -> do
-                token <- fromString `fmap` getEnv "SPTRANS_TOKEN"
-                res <- newOlhoVivoApi
-                           session
-                           def
-                           token
-                res `shouldBe` True
+    describe "authenticateSession" $
+        it "authenticates a given HTTP session" $
+            Wreq.withSession $ \session -> do
+                authenticateSession def token session
+                sessionIsAuthenticated session
+
+    describe "withSession" $
+        it "creates a new authenticated olhovivo `Session` object" $
+           withSession def token $ \session ->
+               sessionIsAuthenticated session
 
     describe "olhoVivoLine" $
-        it "makes a query for lines matching a string" $
-            withSession $ \session -> do
-                token <- fromString `fmap` getEnv "SPTRANS_TOKEN"
-                res <- newOlhoVivoApi session def token
-
-                res' <- olhoVivoLines session def "bandeira"
-                any (\l -> olhovivoLineLetreiro l == "6262" &&
-                           olhovivoLineTipo l == 10) res'
-                    `shouldBe` True
+        it' "makes a query for lines matching a string" $ \session -> do
+            res <- olhoVivoLines session def "bandeira"
+            any (\l -> olhovivoLineLetreiro l == "6262" &&
+                       olhovivoLineTipo l == 10) res
+                `shouldBe` True
 
     describe "olhoVivoStops" $
-        it "makes a query for stops matching a string" $
-            withSession $ \session -> do
-                token <- fromString `fmap` getEnv "SPTRANS_TOKEN"
-                res <- newOlhoVivoApi session def token
-
-                res' <- olhoVivoStops session def "Afonso"
-                any (\s -> olhovivoStopEndereco s ==
-                               "R ARMINDA/ R BALTHAZAR DA VEIGA") res'
-                    `shouldBe` True
+        it' "makes a query for stops matching a string" $ \session -> do
+            res <- olhoVivoStops session def "Afonso"
+            any (\s -> olhovivoStopEndereco s ==
+                       "R ARMINDA/ R BALTHAZAR DA VEIGA") res
+                `shouldBe` True
 
     describe "olhoVivoExpressLanes" $
-        it "gets all express lanes in the city" $
-            withSession $ \session -> do
-                token <- fromString `fmap` getEnv "SPTRANS_TOKEN"
-                res <- newOlhoVivoApi session def token
+        it' "gets all express lanes in the city" $ \session -> do
+            res <- olhoVivoExpressLanes session def
+            any (\s -> olhovivoExpressLaneNome s == "Campo Limpo") res
+                `shouldBe` True
 
-                res' <- olhoVivoExpressLanes session def
-                any (\s -> olhovivoExpressLaneNome s ==
-                               "Campo Limpo") res'
-                    `shouldBe` True
+  where
+    it' title action = it title (withTestSession action)
 
-    -- describe "olhoVivoLinePositions" $
-    --     it "gets all bus positions for a line code" $
-    --         withSession $ \session -> do
-    --             token <- fromString `fmap` getEnv "SPTRANS_TOKEN"
-    --             res <- newOlhoVivoApi session def token
-
-    --             res' <- olhoVivoLinePositions session def 906
-    --             hPrint stderr res'
+sessionIsAuthenticated :: Session -> IO ()
+sessionIsAuthenticated session = do
+    res <- get session (urlForEndpoint def "/Linha/Buscar?termosBusca=bandeira")
+    res ^. responseStatus . statusCode `shouldBe` 200
